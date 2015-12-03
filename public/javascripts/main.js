0998065d9d5a5d2a6051b549baf63d953c869df7
Raphael Schweikert <any@sabberworm.com>
2015-12-03 16:32:17 +0100

    Start implementing form editing commands

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,15 @@
 (function(){
+	function initFormBindings(form) {
+		if(!form) {
+			return;
+		}
+		if(!form.dataset.formData) {
+			return;
+		}
+	}
+	
 	document.addEventListener('DOMContentLoaded', function() {
-
+		var form = document.querySelector('#main form');
+		initFormBindings(form);
 	}, false);
 }());
